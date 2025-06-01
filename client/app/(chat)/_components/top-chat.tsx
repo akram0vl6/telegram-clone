33f6f51a6706a0c77f24ca9e4601edfaa84b0f92
Diyor <akramovdiyor@gmail.com>
2025-06-01 09:10:12 +0600

    ui-02-version

--- client/app/(chat)/_components/top-chat.tsx
+++ client/app/(chat)/_components/top-chat.tsx
@@ -9,7 +9,7 @@ import {
 } from "@/components/ui/sheet";
 import { useCurrentContact } from "@/hooks/use-current";
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
-import { Separator } from "@radix-ui/react-separator";
+import { Separator } from "@/components/ui/separator";
 import { Settings2 } from "lucide-react";
 import React from "react";
 
@@ -19,13 +19,13 @@ const TopChat = () => {
   return (
     <div className="w-full flex items-center justify-between sticky top-0 z-50 h-[8vh] p-2 border-b bg-background">
       <div className="flex items-center">
-        <Avatar className="w-8 h-8 z-10">
+        <Avatar className="w-8 h-8 rounded-full">
           <AvatarImage
             src={currentContact?.avatar}
             alt={currentContact?.email}
-            className="w-8 h-8 object-cover rounded-full"
+            className="w-full h-full object-cover rounded-full"
           />
-          <AvatarFallback className="w-8 h-8 uppercase flex items-center justify-center text-sm">
+          <AvatarFallback className="w-full h-full uppercase flex items-center justify-center text-xs bg-muted rounded-full">
             {currentContact?.email[0]}
           </AvatarFallback>
         </Avatar>
@@ -59,42 +59,57 @@ const TopChat = () => {
         <SheetContent>
           <SheetHeader>
             <SheetTitle />
-					<div className='mx-auto w-1/2 max-md:w-1/4 h-36 relative '>
-						<Avatar className='w-full h-36 rounded-full'>
-							<AvatarImage src={currentContact?.avatar} alt={currentContact?.email} className='object-cover rounded-full' />
-							<AvatarFallback className='text-6xl uppercase font-spaceGrotesk'>{currentContact?.email[0]}</AvatarFallback>
-						</Avatar>
-					</div>
+            <div className="mx-auto w-1/2 max-md:w-1/4 h-36 relative ">
+              <Avatar className="w-4 h-4 rounded-full">
+                <AvatarImage
+                  src={currentContact?.avatar}
+                  alt={currentContact?.email}
+                  className="w-35 h-35 object-cover rounded-full"
+                />
+                <AvatarFallback className="w-full h-full uppercase flex items-center justify-center text-sm bg-muted rounded-full">
+                  {currentContact?.email[0]}
+                </AvatarFallback>
+              </Avatar>
+            </div>
 
-          <Separator className='my-2' />
-          
-          <h1 className='text-center capitalize font-spaceGrotesk text-xl'>{currentContact?.email}</h1>
+            <Separator className="my-2" />
 
-					<div className='flex flex-col space-y-1'>
-						{currentContact?.firstName && (
-							<div className='flex items-center gap-1 mt-4'>
-								<p className='font-spaceGrotesk'>First Name: </p>
-								<p className='font-spaceGrotesk text-muted-foreground'>{currentContact?.firstName}</p>
-							</div>
-						)}
-						{currentContact?.lastName && (
-							<div className='flex items-center gap-1 mt-4'>
-								<p className='font-spaceGrotesk'>Last Name: </p>
-								<p className='font-spaceGrotesk text-muted-foreground'>{currentContact?.lastName}</p>
-							</div>
-						)}
-						{currentContact?.bio && (
-							<div className='flex items-center gap-1 mt-4'>
-								<p className='font-spaceGrotesk'>
-									About: <span className='font-spaceGrotesk text-muted-foreground'>{currentContact?.bio}</span>
-								</p>
-							</div>
-						)}
+            <h1 className="text-center capitalize font-spaceGrotesk text-xl">
+              {currentContact?.email}
+            </h1>
 
-						<Separator className='my-2' />
+            <div className="flex flex-col space-y-1">
+              {currentContact?.firstName && (
+                <div className="flex items-center gap-1 mt-4">
+                  <p className="font-spaceGrotesk">First Name: </p>
+                  <p className="font-spaceGrotesk text-muted-foreground">
+                    {currentContact?.firstName}
+                  </p>
+                </div>
+              )}
+              {currentContact?.lastName && (
+                <div className="flex items-center gap-1 mt-4">
+                  <p className="font-spaceGrotesk">Last Name: </p>
+                  <p className="font-spaceGrotesk text-muted-foreground">
+                    {currentContact?.lastName}
+                  </p>
+                </div>
+              )}
+              {currentContact?.bio && (
+                <div className="flex items-center gap-1 mt-4">
+                  <p className="font-spaceGrotesk">
+                    About:{" "}
+                    <span className="font-spaceGrotesk text-muted-foreground">
+                      {currentContact?.bio}
+                    </span>
+                  </p>
+                </div>
+              )}
 
-						<h2 className='text-xl'>Image</h2>
-						{/* <div className='flex flex-col space-y-2'>
+              <Separator className="my-2" />
+
+              <h2 className="text-xl">Image</h2>
+              {/* <div className='flex flex-col space-y-2'>
 							{messages
 								.filter(msg => msg.image)
 								.map(msg => (
@@ -103,7 +118,7 @@ const TopChat = () => {
 									</div>
 								))}
 						</div> */}
-					</div>
+            </div>
           </SheetHeader>
         </SheetContent>
       </Sheet>
